ffde0921c13cb68f54970457908ca2ce9a136520
Georgii Nazarov <georgii.nazarov14@gmail.com>
2024-12-08 23:36:22 +0300

    Change Code.java

--- src/main/java/com/example/nto/entity/Code.java
+++ src/main/java/com/example/nto/entity/Code.java
@@ -5,11 +5,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Code {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private long id;
+    @Column(name="value")
     private long value;
 }
