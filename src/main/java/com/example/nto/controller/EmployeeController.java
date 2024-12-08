dfb0e4f65dbacf339b5cff4c7f1eb53f8a17429e
Kirill Zolotarevskiy <krillzolotarevskiy62@gmail.com>
2024-12-08 23:50:11 +0300

    Update EmployeeController.java

--- src/main/java/com/example/nto/controller/EmployeeController.java
+++ src/main/java/com/example/nto/controller/EmployeeController.java
@@ -1,8 +1,64 @@
 package com.example.nto.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.nto.entity.Employee;
+import com.example.nto.service.impl.EmployeeServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
+@RequestMapping("/api/")
+@AllArgsConstructor
 public class EmployeeController {
+    private EmployeeServiceImpl service;
+
+
+    @GetMapping("{login}/auth")
+    private ResponseEntity<String> getAuth(@PathVariable("login") String login){
+
+        Employee employee = service.findEmployeeByLogin(login);
+        if(employee!=null){
+            return ResponseEntity.status(200).body("данный логин существует - можно пользоваться приложением");
+
+        }
+        else{
+            return ResponseEntity.status(401).body("логина не существует или неверный");
+        }
+
+
+
+    }
+
+    @GetMapping("{login}/info")
+    private ResponseEntity<Object> getInfo(@PathVariable("login") String login){
+        Employee employee = service.findEmployeeByLogin(login);
+        if(employee!=null){
+            return ResponseEntity.ok(employee);
+
+
+        }
+        else{
+            return ResponseEntity.status(401).body("логина не существует или неверный");
+        }
+
+    }
+
+    @PatchMapping("{login}/open")
+    private ResponseEntity<Object> updateLocalTime(@PathVariable("login") String login){
+        Employee employee = service.findEmployeeByLogin(login);
+        if(employee!=null){
+            employee.setLastVisit(LocalDateTime.now());
+            service.updateLocalTime(employee);
+
+            return ResponseEntity.status(200).body("дверь открылась");
+        }
+        else {
+            return ResponseEntity.status(401).body("логина не существует или неверный");
+        }
+    }
+
+
 }
