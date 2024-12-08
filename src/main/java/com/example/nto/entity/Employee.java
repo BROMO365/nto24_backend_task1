5b7bd7fb58905de7b113f92aff2ab7ce0aede68a
Kirill Zolotarevskiy <krillzolotarevskiy62@gmail.com>
2024-12-08 21:55:31 +0300

    all

--- src/main/java/com/example/nto/entity/Employee.java
+++ src/main/java/com/example/nto/entity/Employee.java
@@ -5,17 +5,31 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name="employee")
 public class Employee {
+    @Id
     private long id;
+
+    @Column(name = "login")
     private String login;
+
+    @Column(name="name")
     private String name;
+
+    @Column(name = "photo")
     private String photo;
+
+    @Column(name="position")
     private String position;
+
+    @Column(name="lastVisit")
     private LocalDateTime lastVisit;
 }
--- src/main/java/com/example/nto/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/nto/service/impl/EmployeeServiceImpl.java
@@ -3,4 +3,5 @@ package com.example.nto.service.impl;
 import com.example.nto.service.EmployeeService;
 
 public class EmployeeServiceImpl implements EmployeeService {
+
 }
